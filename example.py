79d91c1a056a55ecbe2259625fde6471459a958c
Taiki Hibira <thibira@amazon.co.jp>
2024-06-24 20:54:15 +0900

    init

--- example.py
+++ example.py
@@ -1,10 +1,10 @@
-# Author thibira@amazon.co.jp
+# Author hibira
 # ローカルで実行する場合のサンプルコードです。
 
 import os
 import app
 
-os.environ['AURORA_CLUSTER'] = 'aws-rails-sample'
+os.environ['AURORA_CLUSTER'] = 'xxxxxx'
 os.environ['THRESHOLD_TOTAL_LOG_FILE_SIZE'] = str(0.001 * 0.15)
 os.environ['METRICS_NAMESPACE'] = 'CUSTOM/RDS'
 os.environ['TOTAL_LOG_FILE_SIZE_METRICS_NAME'] = 'TotalLogFileSize'
